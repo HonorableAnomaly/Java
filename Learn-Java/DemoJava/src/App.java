9156c10c9e70c9a67a89131c761c402194bb0dbc
Ricky Argenbright <honorable.anomaly@gmail.com>
2024-02-01 12:10:07 -0800

    Create DemoJava App
    * Created Sum class
    
    * Added two numbers together and printed them
    
    * Called Sum.sum in main

---
+++ Learn-Java/DemoJava/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Sum.sum();
+
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Learn-Java/DemoJava/src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void sum() {
+        
+        int num1 = 7;
+        int num2 = 5;
+        int sum = num1 * num2;
+
+        System.out.println(sum);
+    }
+}
